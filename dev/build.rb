b655cba58b9f2ab26110dd8f4be26bf13bbef148
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2013-07-31 07:10:16 +0000

    Build scripts modified (references to mbapi_4 and so_5_transport removed).
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.1@41 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/build.rb
+++ dev/build.rb
@@ -1,21 +1,18 @@
 require 'mxx_ru/cpp'
 
-Mxx_ru::setup_target(
-	Mxx_ru::Cpp::Composite_target.new( Mxx_ru::BUILD_ROOT ) {
+MxxRu::Cpp::composite_target( MxxRu::BUILD_ROOT ) {
 
-		toolset.force_cpp0x_std
-		global_include_path "."
+	toolset.force_cpp0x_std
+	global_include_path "."
 
-		if toolset.name == 'gcc' && toolset.tag('gcc_port', 'unix') == 'cygwin'
-			global_define "ACE_HAS_CUSTOM_EXPORT_MACROS=0"
-		end
-
+	# If there is local options file then use it.
+	if FileTest.exist?( "local-build.rb" )
+		required_prj "local-build.rb"
+	else
 		default_runtime_mode( MxxRu::Cpp::RUNTIME_RELEASE )
 		MxxRu::enable_show_brief
+	end
 
-		required_prj( "ace/ace_lib_unpacker.rb" )
-		required_prj( "so_5/prj.rb" )
-		required_prj( "so_5_transport/prj.rb" )
-		required_prj( "mbapi_4/prj.rb" )
-	}
-)
+	required_prj( "ace/ace_lib_unpacker.rb" )
+	required_prj( "so_5/prj.rb" )
+}
--- dev/build_all.rb
+++ dev/build_all.rb
@@ -4,10 +4,6 @@ MxxRu::Cpp::composite_target {
 	required_prj 'build.rb'
 
 	required_prj 'test/so_5/build_tests.rb'
-	required_prj 'test/so_5_transport/build_tests.rb'
-	required_prj 'test/mbapi_4/build_tests.rb'
 
 	required_prj 'sample/so_5/build_samples.rb'
-	required_prj 'sample/so_5_transport/build_samples.rb'
-	required_prj 'sample/mbapi_4/build_samples.rb'
 }
