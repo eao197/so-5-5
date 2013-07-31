d4acd2138b4d281a78db0eb37e497feb22039717
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2013-07-31 06:35:15 +0000

    Tests of so_5_transport subproject moved to new location.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.1@33 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5_transport/build_tests.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/ruby
-require 'mxx_ru/cpp'
-
-Mxx_ru::setup_target(
-	Mxx_ru::Cpp::Composite_target.new( "test/so_5_transport/build_tests.rb" ) {
-		required_prj( "test/so_5_transport/scattered_block_stream/prj.ut.rb" )
-		required_prj( "test/so_5_transport/channel_stream/prj.ut.rb" )
-	}
-)
--- dev/test/so_5_transport/scattered_block_stream/prj.ut.rb
+++
@@ -1,7 +0,0 @@
-require 'mxx_ru/binary_unittest'
-
-Mxx_ru::setup_target(
-	Mxx_ru::Binary_unittest_target.new(
-		"test/so_5_transport/scattered_block_stream/prj.ut.rb",
-		"test/so_5_transport/scattered_block_stream/prj.rb" )
-)
