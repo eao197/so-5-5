845a25438b33d4c998dd755384529081c1d88c45
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2013-07-31 06:34:10 +0000

    Samples of so_5_transport subproject moved to new location.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.1@32 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/sample/so_5_transport/build_samples.rb
+++
@@ -1,7 +0,0 @@
-require 'mxx_ru/cpp'
-
-Mxx_ru::setup_target(
-	Mxx_ru::Cpp::Composite_target.new( "sample/so_5_transport/build_samples.rb" ) {
-		required_prj( "sample/so_5_transport/raw_channel/prj.rb" )
-	}
-)
--- dev/sample/so_5_transport/raw_channel/prj.rb
+++
@@ -1,8 +0,0 @@
-require 'mxx_ru/cpp'
-
-Mxx_ru::setup_target(
-	Mxx_ru::Cpp::Composite_target.new( "sample/so_5_transport/raw_channel/prj.rb" ) {
-		required_prj( "sample/so_5_transport/raw_channel/tcp_srv.rb" )
-		required_prj( "sample/so_5_transport/raw_channel/tcp_cln.rb" )
-	}
-)
