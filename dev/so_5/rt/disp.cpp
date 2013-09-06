404f5c45a5a64654a927087b2a973a79f3994395
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2013-09-06 17:44:26 +0000

    Small code fix.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.1@198 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/so_5/rt/disp.cpp
+++ dev/so_5/rt/disp.cpp
@@ -4,6 +4,8 @@
 
 #include <so_5/rt/h/disp.hpp>
 
+#include <so_5/h/exception.hpp>
+
 namespace so_5
 {
 
@@ -40,6 +42,12 @@ dispatcher_t::set_disp_event_exception_handler(
 disp_evt_except_handler_t &
 dispatcher_t::query_disp_evt_except_handler()
 {
+	if( !m_disp_evt_except_handler )
+		throw so_5::exception_t(
+				"call to dispatcher_t::query_disp_evt_except_handler() when "
+				"evt_except_handler is null",
+				so_5::rc_unexpected_error );
+
 	return *m_disp_evt_except_handler;
 }
 
