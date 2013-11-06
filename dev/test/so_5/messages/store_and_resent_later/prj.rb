699df371ce311ccb9215296c20438dcbc1ffb8a1
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2013-11-06 08:54:16 +0000

    Another unit test added.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.2@375 1d1731ae-3f85-447d-88f8-c72b288064d1

---
+++ dev/test/so_5/messages/store_and_resent_later/prj.rb
@@ -0,0 +1,12 @@
+require 'mxx_ru/cpp'
+
+MxxRu::Cpp::exe_target {
+
+	required_prj( "ace/dll.rb" )
+	required_prj( "so_5/prj.rb" )
+
+	target( "_unit.test.messages.store_and_resend_later" )
+
+	cpp_source( "main.cpp" )
+}
+
