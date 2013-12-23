94e10640f6c4c1a1c9562973f33733788972efb7
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2013-12-23 13:21:06 +0000

    Small prj.rb adoption for GCC on Linux
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.2.4-microbenchmarking@508 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/microbenchmarks/so_5/demand_queue_1_bench_1_stdcpp/prj.rb
+++ dev/microbenchmarks/so_5/demand_queue_1_bench_1_stdcpp/prj.rb
@@ -4,5 +4,10 @@ MxxRu::Cpp::exe_target {
 	target "_microbench.demand_queue_1_bench_1_stdcpp"
 
 	cpp_source "main.cpp"
+
+	if 'gcc' == toolset.name
+		compiler_option '-pthread'
+		linker_option '-pthread'
+	end
 }
 
