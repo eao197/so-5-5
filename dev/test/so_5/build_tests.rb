438406cd4609e5428494db382e7dd1db2b57e33d
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2014-06-09 11:18:30 +0000

    Another unit-test for svc_handlers.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.3@581 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/build_tests.rb
+++ dev/test/so_5/build_tests.rb
@@ -56,4 +56,5 @@ MxxRu::Cpp::composite_target {
 	required_prj( "test/so_5/svc/svc_handler_exception/prj.ut.rb" )
 	required_prj( "test/so_5/svc/no_svc_handlers/prj.ut.rb" )
 	required_prj( "test/so_5/svc/several_svc_handlers/prj.ut.rb" )
+	required_prj( "test/so_5/svc/svc_handler_not_called/prj.ut.rb" )
 }
