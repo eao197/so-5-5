4da637f38115e6eac4e3ef0f7a7f6af5e2619fd8
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2014-07-09 13:09:09 +0000

    A new unit-test for checking correctness of cooperation registration/deregistration synchronization correctness.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.4-unstable--coop-manager@671 1d1731ae-3f85-447d-88f8-c72b288064d1

---
+++ dev/test/so_5/coop/parent_child_4/prj.rb
@@ -0,0 +1,12 @@
+require 'mxx_ru/cpp'
+
+MxxRu::Cpp::exe_target {
+
+	required_prj "ace/dll.rb"
+	required_prj "so_5/prj.rb"
+
+	target "_unit.test.coop.parent_child_4"
+
+	cpp_source "main.cpp"
+}
+
