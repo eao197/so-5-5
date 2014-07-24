90083f4bb978166aa6d63a79fd01e1c4a217866a
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2014-07-24 12:56:20 +0000

    The first set of unit-tests for adv_thread_pool dispatcher. They are copied from unit-tests for thread_pool dispatcher.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.4-unstable--new-disp@749 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/build_tests.rb
+++ dev/test/so_5/build_tests.rb
@@ -26,6 +26,10 @@ MxxRu::Cpp::composite_target {
 	required_prj( "test/so_5/disp/thread_pool/cooperation_fifo/prj.ut.rb" )
 	required_prj( "test/so_5/disp/thread_pool/individual_fifo/prj.ut.rb" )
 
+	required_prj( "test/so_5/disp/adv_thread_pool/simple/prj.ut.rb" )
+	required_prj( "test/so_5/disp/adv_thread_pool/cooperation_fifo/prj.ut.rb" )
+	required_prj( "test/so_5/disp/adv_thread_pool/individual_fifo/prj.ut.rb" )
+
 	required_prj( "test/so_5/event_handler/subscribe_errors/prj.ut.rb" )
 	required_prj( "test/so_5/event_handler/ignore_exception/prj.ut.rb" )
 	required_prj( "test/so_5/event_handler/exception_reaction_inheritance/prj.ut.rb" )
