03a2a18638f68c693a816c540b06a92e436bb7f6
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2014-07-30 05:53:36 +0000

    event_queue_proxy_t now uses default_rw_spinlock_t for synchronization.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.4-unstable@775 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/so_5/rt/h/event_queue_proxy.hpp
+++ dev/so_5/rt/h/event_queue_proxy.hpp
@@ -11,6 +11,10 @@
 #if !defined( _SO_5__RT__EVENT_QUEUE_PROXY_HPP_ )
 #define _SO_5__RT__EVENT_QUEUE_PROXY_HPP_
 
+#include <mutex>
+
+#include <so_5/h/spinlocks.hpp>
+
 #include <so_5/rt/h/event_queue.hpp>
 #include <so_5/rt/h/atomic_refcounted.hpp>
 
@@ -31,8 +35,6 @@ class event_queue_proxy_t : private atomic_refcounted_t
 	public :
 		event_queue_proxy_t()
 			{
-				m_lock.clear( std::memory_order_release );
-				m_workers = 0;
 				m_queue_ptr = nullptr;
 			}
 
@@ -57,29 +59,16 @@ class event_queue_proxy_t : private atomic_refcounted_t
 		inline void
 		push( execution_demand_t demand )
 			{
-				lock();
+				read_lock_guard_t< default_rw_spinlock_t > l( m_lock );
 
 				event_queue_t * q = m_queue_ptr.load( std::memory_order_consume );
 				if( q )
-					{
-						workers_count_guard_t guard( m_workers );
-						unlock();
-
-						q->push( std::move( demand ) );
-					}
-				else
-					unlock();
+					q->push( std::move( demand ) );
 			}
 
 	private :
 		//! Object's lock.
-		/*!
-		 * Is used as a spinlock.
-		 */
-		std::atomic_flag m_lock;
-
-		//! Current workers count.
-		std::atomic_uint m_workers;
+		default_rw_spinlock_t m_lock;
 
 		//! A pointer to the actual event_queue.
 		/*!
@@ -87,57 +76,14 @@ class event_queue_proxy_t : private atomic_refcounted_t
 		 */
 		std::atomic< event_queue_t * > m_queue_ptr;
 
-		//! A helper class for insure that workers count increments
-		//! are in pair with decrements.
-		struct workers_count_guard_t
-			{
-				std::atomic_uint & m_value;
-
-				inline workers_count_guard_t( std::atomic_uint & v )
-					:	m_value( v )
-					{
-						++m_value;
-					}
-				inline ~workers_count_guard_t()
-					{
-						--m_value;
-					}
-			};
-
-		//! Lock object.
-		inline void
-		lock()
-			{
-				while( m_lock.test_and_set( std::memory_order_acquire ) )
-					;
-			}
-
-		//! Unlock object.
-		inline void
-		unlock()
-			{
-				m_lock.clear( std::memory_order_release );
-			}
-
-		//! Wait until all worker finished their work.
-		inline void
-		wait_all_workers_gone()
-			{
-				while( 0 != m_workers.load( std::memory_order_acquire ) )
-					;
-			}
-
 		//! Setup a pointer to queue.
 		inline event_queue_t *
 		setup_queue_ptr( event_queue_t * queue )
 			{
-				lock();
-				wait_all_workers_gone();
+				std::lock_guard< default_rw_spinlock_t > l( m_lock );
 
 				auto r = m_queue_ptr.load( std::memory_order_consume );
 				m_queue_ptr.store( queue, std::memory_order_release );
-
-				unlock();
 
 				return r;
 			}
