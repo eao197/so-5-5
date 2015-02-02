5d013a5ce71c8d880a31879a1deb12aca43aecd6
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-02-02 12:36:03 +0000

    Modification of one of the unit tests to control the correctness of destroy_all_subscriptions implementations.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5--agent-tunung-options@1113 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/mbox/drop_subscription/main.cpp
+++ dev/test/so_5/mbox/drop_subscription/main.cpp
@@ -9,6 +9,90 @@
 
 #include <various_helpers_1/time_limited_execution.hpp>
 
+class test_mbox_t : public so_5::rt::abstract_message_box_t
+	{
+	private :
+		const so_5::rt::mbox_t m_actual_mbox;
+
+		unsigned int m_subscriptions = 0;
+		unsigned int m_unsubscriptions = 0;
+
+	public :
+		test_mbox_t( so_5::rt::environment_t & env )
+			:	m_actual_mbox( env.create_local_mbox() )
+			{
+			}
+
+		virtual ~test_mbox_t()
+			{
+				if( m_subscriptions != m_unsubscriptions )
+					{
+						std::cerr << "subscriptions(" << m_subscriptions
+								<< ") != unsubscriptions(" << m_unsubscriptions
+								<< "). Test aborted!" << std::endl;
+
+						std::abort();
+					}
+			}
+
+		virtual so_5::mbox_id_t
+		id() const
+			{
+				return m_actual_mbox->id();
+			}
+
+		virtual void
+		deliver_message(
+			const std::type_index & type_index,
+			const so_5::rt::message_ref_t & message_ref ) const
+			{
+				m_actual_mbox->deliver_message( type_index, message_ref );
+			}
+
+		virtual void
+		deliver_service_request(
+			const std::type_index & type_index,
+			const so_5::rt::message_ref_t & svc_request_ref ) const
+			{
+				m_actual_mbox->deliver_service_request(
+						type_index,
+						svc_request_ref );
+			}
+
+		virtual void
+		subscribe_event_handler(
+			const std::type_index & type_index,
+			so_5::rt::agent_t * subscriber )
+			{
+				++m_subscriptions;
+				m_actual_mbox->subscribe_event_handler( type_index, subscriber );
+			}
+
+		virtual void
+		unsubscribe_event_handlers(
+			const std::type_index & type_index,
+			so_5::rt::agent_t * subscriber )
+			{
+				++m_unsubscriptions;
+				m_actual_mbox->unsubscribe_event_handlers( type_index, subscriber );
+			}
+
+		virtual std::string
+		query_name() const { return m_actual_mbox->query_name(); }
+
+		virtual so_5::rt::mbox_type_t
+		type() const override
+			{
+				return m_actual_mbox->type();
+			}
+
+		static so_5::rt::mbox_t
+		create( so_5::rt::environment_t & env )
+			{
+				return so_5::rt::mbox_t( new test_mbox_t( env ) );
+			}
+	};
+
 struct msg_one : public so_5::rt::signal_t {};
 struct msg_two : public so_5::rt::signal_t {};
 struct msg_three : public so_5::rt::signal_t {};
@@ -31,7 +115,7 @@ class a_test_t : public so_5::rt::agent_t
 					so_5::rt::agent_t::tuning_options()
 						.subscription_storage_factory( std::move( factory ) ) )
 			,	m_sequence( sequence )
-			,	m_mbox( env.create_local_mbox() )
+			,	m_mbox( test_mbox_t::create( env ) )
 		{
 		}
 
