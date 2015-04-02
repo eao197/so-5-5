d9fcdfcd5f48b435259d071b92a93b9ac55250cf
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-04-02 16:42:02 +0000

    Fix error in ruby script detected under ruby-2.2.1
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.4--internal-stats@1304 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/samples_as_unit_tests/details.rb
+++ dev/test/so_5/samples_as_unit_tests/details.rb
@@ -1,7 +1,7 @@
 require 'mxx_ru/binary_unittest'
 
 def setup_sample_as_unit_test(sample_name = nil)
-  ut_name = File.basename( caller(1,1)[0][ /^(.+):\d/ ] )
+  ut_name = File.basename( /^(.+):\d/.match( caller(1,1)[0] )[1] )
   sample_name = File.basename( ut_name, '.ut.rb' ) unless sample_name
 
   ut_path     = 'test/so_5/samples_as_unit_tests'
