02255399c3d5f304d662b4eff88bcb2dd99faaef
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-04-03 06:25:52 +0000

    Small refactoring of helper script.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.4--internal-stats@1306 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/samples_as_unit_tests/details.rb
+++ dev/test/so_5/samples_as_unit_tests/details.rb
@@ -4,12 +4,9 @@ def setup_sample_as_unit_test(sample_name = nil)
   ut_name = File.basename( /^(.+):\d/.match( caller(1,1)[0] )[1] )
   sample_name = File.basename( ut_name, '.ut.rb' ) unless sample_name
 
-  ut_path     = 'test/so_5/samples_as_unit_tests'
-  sample_path = "sample/so_5/#{sample_name}"
-
   MxxRu::setup_target(
     MxxRu::BinaryUnittestTarget.new(
-      "#{ut_path}/#{ut_name}",
-      "#{sample_path}/prj.rb" )
+      "test/so_5/samples_as_unit_tests/#{ut_name}",
+      "sample/so_5/#{sample_name}/prj.rb" )
   )
 end
