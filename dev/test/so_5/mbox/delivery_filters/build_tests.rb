31dae26b56b8f4bba89f26073f27d2ef5f4e259c
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-04-27 12:59:59 +0000

    Another unit-test for delivery filters.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.5--msg-filters@1449 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/mbox/delivery_filters/build_tests.rb
+++ dev/test/so_5/mbox/delivery_filters/build_tests.rb
@@ -10,4 +10,5 @@ MxxRu::Cpp::composite_target {
 	required_prj "#{path}/filter_on_mpsc_mbox/prj.ut.rb"
 	required_prj "#{path}/dereg_subscriber/prj.ut.rb"
 	required_prj "#{path}/set_unset/prj.ut.rb"
+	required_prj "#{path}/exception_in_filter/prj.ut.rb"
 }
