4ce6e8b6d6a77766e601f99967a0ab48c92a16f7
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-08-07 11:12:11 +0000

    A second iteration on prio::common_thread dispatcher implementation.
    The simpliest version of dispatcher is implemented and successfully compiled by MSVS2015.
    There is no run-time monitoring data sources yet.
    
    
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.8--dev-trunk@1553 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/so_5/disp/prio/common_thread/pub.cpp
+++ dev/so_5/disp/prio/common_thread/pub.cpp
@@ -14,6 +14,8 @@
 #include <so_5/disp/prio/common_thread/impl/h/demand_queue.hpp>
 #include <so_5/disp/prio/common_thread/impl/h/work_thread.hpp>
 
+#include <so_5/disp/reuse/h/disp_binder_helpers.hpp>
+
 namespace so_5 {
 
 namespace disp {
@@ -21,6 +23,8 @@ namespace disp {
 namespace prio {
 
 namespace common_thread {
+
+namespace impl {
 
 //
 // dispatcher_t
@@ -89,79 +93,256 @@ class dispatcher_t : public so_5::rt::dispatcher_t
 
 	private:
 		//! Demand queue for the dispatcher.
-		impl::demand_queue_t m_demand_queue;
+		demand_queue_t m_demand_queue;
 
 		//! Working thread for the dispatcher.
-		impl::work_thread_t m_work_thread;
+		work_thread_t m_work_thread;
 	};
 
-//FIXME: implement this!
+//
+// binding_actions_mixin_t
+//
+/*!
+ * \since v.5.5.8
+ * \brief Implementation of binding actions to be reused
+ * in various binder implementation.
+ */
+class binding_actions_mixin_t
+	{
+	protected :
+		inline static so_5::rt::disp_binding_activator_t
+		do_bind(
+			dispatcher_t & disp,
+			so_5::rt::agent_ref_t agent,
+			priority_t priority )
+			{
+				auto result = [agent, &disp, priority]() {
+					agent->so_bind_to_dispatcher(
+							*(disp.get_agent_binding( priority )) );
+				};
+
+//FIXME: uncomment this when run-time data stats will be implemented.
 #if 0
+				// Dispatcher must know about yet another agent bound.
+				disp.agent_bound();
+#endif
+
+				return result;
+			}
+
+		inline static void
+		do_unbind(
+			dispatcher_t & disp )
+			{
+//FIXME: uncomment this when run-time data stats will be implemented.
+#if 0
+				// Dispatcher must know about yet another agent bound.
+				disp.agent_unbound();
+#endif
+			}
+	};
+
+//
+// disp_binder_t
+//
+/*!
+ * \since v.5.5.8
+ * \brief Dispatcher binder for dispatcher.
+ */
+class disp_binder_t
+	:	public so_5::rt::disp_binder_t
+	,	protected binding_actions_mixin_t
+	{
+	public:
+		disp_binder_t(
+			std::string disp_name,
+			priority_t priority )
+			:	m_disp_name( std::move( disp_name ) )
+			,	m_priority( priority )
+			{}
+
+		virtual so_5::rt::disp_binding_activator_t
+		bind_agent(
+			so_5::rt::environment_t & env,
+			so_5::rt::agent_ref_t agent ) override
+			{
+				using namespace so_5::disp::reuse;
+
+				return do_with_dispatcher< dispatcher_t >(
+					env,
+					m_disp_name,
+					[this, agent]( dispatcher_t & disp )
+					{
+						return do_bind( disp, std::move( agent ), m_priority );
+					} );
+			}
+
+		virtual void
+		unbind_agent(
+			so_5::rt::environment_t & env,
+			so_5::rt::agent_ref_t agent ) override
+			{
+				using namespace so_5::disp::reuse;
+
+				do_with_dispatcher< dispatcher_t >( env, m_disp_name,
+					[agent]( dispatcher_t & disp )
+					{
+						do_unbind( disp );
+					} );
+			}
+
+	private:
+		//! Name of the dispatcher to be bound to.
+		const std::string m_disp_name;
+		//! Priority to be used for binding.
+		const priority_t m_priority;
+	};
+
+//
+// private_dispatcher_binder_t
+//
+
+/*!
+ * \since v.5.5.8
+ * \brief A binder for the private %common_thread dispatcher.
+ */
+class private_dispatcher_binder_t
+	:	public so_5::rt::disp_binder_t
+	,	private binding_actions_mixin_t
+	{
+	public:
+		explicit private_dispatcher_binder_t(
+			//! A handle for private dispatcher.
+			//! It is necessary to manage lifetime of the dispatcher instance.
+			private_dispatcher_handle_t handle,
+			//! A dispatcher instance to work with.
+			dispatcher_t & instance,
+			//! A priority to be used during binding.
+			priority_t priority )
+			:	m_handle( std::move( handle ) )
+			,	m_instance( instance )
+			,	m_priority( priority )
+			{}
+
+		virtual so_5::rt::disp_binding_activator_t
+		bind_agent(
+			so_5::rt::environment_t & /* env */,
+			so_5::rt::agent_ref_t agent ) override
+			{
+				return do_bind( m_instance, std::move( agent ), m_priority );
+			}
+
+		virtual void
+		unbind_agent(
+			so_5::rt::environment_t & /*env*/,
+			so_5::rt::agent_ref_t /*agent_ref*/ ) override
+			{
+				do_unbind( m_instance );
+			}
+
+	private:
+		//! A handle for private dispatcher.
+		/*!
+		 * It is necessary to manage lifetime of the dispatcher instance.
+		 */
+		private_dispatcher_handle_t m_handle;
+		//! A dispatcher instance to work with.
+		dispatcher_t & m_instance;
+
+		//! A priority to be used during binding.
+		const priority_t m_priority;
+	};
+
+//
+// real_private_dispatcher_t
+//
+/*!
+ * \since v.5.5.8
+ * \brief A real implementation of private_dispatcher interface.
+ */
+class real_private_dispatcher_t : public private_dispatcher_t
+	{
+	public :
+		/*!
+		 * Constructor creates a dispatcher instance and launces it.
+		 */
+		real_private_dispatcher_t(
+			//! SObjectizer Environment to work in.
+			so_5::rt::environment_t & env,
+			//! Value for creating names of data sources for
+			//! run-time monitoring.
+			const std::string & data_sources_name_base )
+			:	m_disp( new dispatcher_t() )
+			{
+				m_disp->set_data_sources_name_base( data_sources_name_base );
+				m_disp->start( env );
+			}
+
+		/*!
+		 * Destructors shuts an instance down and waits for it.
+		 */
+		~real_private_dispatcher_t()
+			{
+				m_disp->shutdown();
+				m_disp->wait();
+			}
+
+		virtual so_5::rt::disp_binder_unique_ptr_t
+		binder( priority_t priority ) override
+			{
+				return so_5::rt::disp_binder_unique_ptr_t(
+						new private_dispatcher_binder_t(
+								private_dispatcher_handle_t( this ),
+								*m_disp,
+								priority ) );
+			}
+
+	private :
+		std::unique_ptr< dispatcher_t > m_disp;
+	};
+
+} /* namespace impl */
+
 //
 // private_dispatcher_t
 //
 
-/*!
- * \since v.5.5.8
- * \brief An interface for %common_thread private dispatcher.
- */
-class SO_5_TYPE private_dispatcher_t : public so_5::atomic_refcounted_t
-	{
-	public :
-		virtual ~private_dispatcher_t();
+private_dispatcher_t::~private_dispatcher_t()
+	{}
 
-		//! Create a binder for that private dispatcher.
-		virtual so_5::rt::disp_binder_unique_ptr_t
-		binder(
-			//! Priority of agent to be bound.
-			priority_t agent_priority ) = 0;
-	};
-
-/*!
- * \since v.5.5.8
- * \brief A handle for the %one_thread private dispatcher.
- */
-using private_dispatcher_handle_t =
-	so_5::intrusive_ptr_t< private_dispatcher_t >;
-
-//! Create a dispatcher.
+//
+// create_disp
+//
 SO_5_FUNC so_5::rt::dispatcher_unique_ptr_t
-create_disp();
+create_disp()
+	{
+		return so_5::rt::dispatcher_unique_ptr_t( new impl::dispatcher_t() );
+	}
 
-/*!
- * \since v.5.5.8
- * \brief Create a private %common_thread dispatcher.
- *
- * \par Usage sample
-\code
-auto common_thread_disp = so_5::disp::prio::common_thread::create_private_disp(
-	env,
-	"request_processor" );
-
-auto coop = env.create_coop( so_5::autoname,
-	// The main dispatcher for that coop will be
-	// private common_thread dispatcher.
-	// Main binder will use the lowest priority for agents.
-	common_thread_disp->binder( so_5::disp::prio::p0 ) );
-\endcode
- */
+//
+// create_private_disp
+//
 SO_5_FUNC private_dispatcher_handle_t
 create_private_disp(
-	//! SObjectizer Environment to work in.
 	so_5::rt::environment_t & env,
-	//! Value for creating names of data sources for
-	//! run-time monitoring.
-	const std::string & data_sources_name_base );
+	const std::string & data_sources_name_base )
+	{
+		return private_dispatcher_handle_t(
+				new impl::real_private_dispatcher_t(
+						env, data_sources_name_base ) );
+	}
 
-//! Create a dispatcher binder object.
+//
+// create_disp_binder
+//
 SO_5_FUNC so_5::rt::disp_binder_unique_ptr_t
 create_disp_binder(
-	//! Name of the dispatcher to be bound to.
 	const std::string & disp_name,
-	//! Priority for agents to be bound.
-	priority_t priority );
-
-#endif
+	priority_t priority )
+	{
+		return so_5::rt::disp_binder_unique_ptr_t( 
+			new impl::disp_binder_t( disp_name, priority ) );
+	}
 
 } /* namespace common_thread */
 
