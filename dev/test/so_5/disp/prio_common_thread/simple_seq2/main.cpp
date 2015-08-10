0ad3a73c1039736e01ddfc743db8d2c7c1017055
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-08-10 15:48:12 +0000

    Another unit-test for prio::common_thread added.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.8--dev-trunk--prio-move@1564 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/disp/prio_common_thread/simple_seq2/main.cpp
+++ dev/test/so_5/disp/prio_common_thread/simple_seq2/main.cpp
@@ -1,5 +1,6 @@
 /*
  * A test of simple sequence of messages for prio::common_thread dispatcher.
+ * A starting msg_hello is sent from on_start() action from the same coop.
  */
 
 #include <so_5/all.hpp>
@@ -11,12 +12,11 @@ struct msg_hello : public so_5::rt::signal_t {};
 void
 define_receiver_agent(
 	so_5::rt::agent_coop_t & coop,
-	so_5::disp::prio::common_thread::private_dispatcher_t & disp,
 	so_5::priority_t priority,
 	const so_5::rt::mbox_t & common_mbox,
 	std::string & sequence )
 	{
-		coop.define_agent( coop.make_agent_context() + priority, disp.binder() )
+		coop.define_agent( coop.make_agent_context() + priority )
 			.event< msg_hello >(
 				common_mbox,
 				[priority, &sequence] {
@@ -28,12 +28,14 @@ define_receiver_agent(
 std::string &
 define_main_agent(
 	so_5::rt::agent_coop_t & coop,
-	so_5::disp::prio::common_thread::private_dispatcher_t & disp,
 	const so_5::rt::mbox_t & common_mbox )
 	{
 		auto sequence = std::make_shared< std::string >();
 
-		coop.define_agent( coop.make_agent_context() + so_5::prio::p0, disp.binder() )
+		coop.define_agent( coop.make_agent_context() + so_5::prio::p0 )
+			.on_start( [common_mbox] {
+					so_5::send< msg_hello >( common_mbox );
+				} )
 			.event< msg_hello >(
 				common_mbox,
 				[&coop, sequence] {
@@ -49,49 +51,20 @@ define_main_agent(
 	}
 
 void
-define_starter_agent(
-	so_5::rt::agent_coop_t & coop,
-	so_5::disp::prio::common_thread::private_dispatcher_t & disp )
-	{
-		coop.define_agent( coop.make_agent_context() + so_5::prio::p0, disp.binder() )
-			.on_start( [&coop, &disp] {
-				auto common_mbox = coop.environment().create_local_mbox();
-
-				coop.environment().introduce_coop(
-					[&]( so_5::rt::agent_coop_t & child )
-					{
-						using namespace so_5::prio;
-
-						std::string & sequence = define_main_agent(
-								child, disp, common_mbox );
-						define_receiver_agent(
-								child, disp, p1, common_mbox, sequence );
-						define_receiver_agent(
-								child, disp, p2, common_mbox, sequence );
-						define_receiver_agent(
-								child, disp, p3, common_mbox, sequence );
-						define_receiver_agent(
-								child, disp, p4, common_mbox, sequence );
-						define_receiver_agent(
-								child, disp, p5, common_mbox, sequence );
-						define_receiver_agent(
-								child, disp, p6, common_mbox, sequence );
-						define_receiver_agent(
-								child, disp, p7, common_mbox, sequence );
-					} );
-
-				so_5::send< msg_hello >( common_mbox );
-			} );
-	}
-
-void
 fill_coop(
 	so_5::rt::agent_coop_t & coop )
 	{
-		using namespace so_5::disp::prio::common_thread;
+		using namespace so_5::prio;
 
-		define_starter_agent( coop,
-			*(create_private_disp(coop.environment())) );
+		auto common_mbox = coop.environment().create_local_mbox();
+		std::string & sequence = define_main_agent( coop, common_mbox );
+		define_receiver_agent( coop, p1, common_mbox, sequence );
+		define_receiver_agent( coop, p2, common_mbox, sequence );
+		define_receiver_agent( coop, p3, common_mbox, sequence );
+		define_receiver_agent( coop, p4, common_mbox, sequence );
+		define_receiver_agent( coop, p5, common_mbox, sequence );
+		define_receiver_agent( coop, p6, common_mbox, sequence );
+		define_receiver_agent( coop, p7, common_mbox, sequence );
 	}
 
 int
@@ -109,7 +82,10 @@ main()
 					so_5::launch(
 						[]( so_5::rt::environment_t & env )
 						{
-							env.introduce_coop( fill_coop );
+							using namespace so_5::disp::prio::common_thread;
+							env.introduce_coop(
+								create_private_disp( env )->binder(),
+								fill_coop );
 						} );
 				},
 				5,
