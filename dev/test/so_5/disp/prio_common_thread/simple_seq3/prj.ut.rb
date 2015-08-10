369ae55a739391935c805e63b39ff09fc968bcbd
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-08-10 15:56:21 +0000

    Yet another unit test for prio::common_thread.
    NOTE: this test detects some error in prio::common_thread dispatcher.
    And this error is not fixed yet!
    
    
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.8--dev-trunk--prio-move@1565 1d1731ae-3f85-447d-88f8-c72b288064d1

---
+++ dev/test/so_5/disp/prio_common_thread/simple_seq3/prj.ut.rb
@@ -0,0 +1,9 @@
+require 'mxx_ru/binary_unittest'
+
+path = 'test/so_5/disp/prio_common_thread/simple_seq3'
+
+Mxx_ru::setup_target(
+	Mxx_ru::Binary_unittest_target.new(
+		"#{path}/prj.ut.rb",
+		"#{path}/prj.rb" )
+)
