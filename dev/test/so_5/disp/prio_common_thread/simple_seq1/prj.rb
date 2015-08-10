5f174b40cc44af48e305db81868ebe8066dfacaa
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-08-10 17:50:56 +0000

    Name of unit-test changed.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.8--dev-trunk--prio-move@1567 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/disp/prio_common_thread/simple_seq1/prj.rb
+++ dev/test/so_5/disp/prio_common_thread/simple_seq1/prj.rb
@@ -4,7 +4,7 @@ MxxRu::Cpp::exe_target {
 
 	required_prj 'so_5/prj.rb'
 
-	target '_unit.test.disp.prio_common_thread.simple_seq'
+	target '_unit.test.disp.prio_common_thread.simple_seq1'
 
 	cpp_source 'main.cpp'
 }
