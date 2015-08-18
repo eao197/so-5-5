e98afa0304b4cfb734739b84e8670ffd551cd84f
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-08-18 16:23:14 +0000

    The first more or less complete version of new example.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.8--dev-trunk@1624 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/sample/so_5/news_board/main.cpp
+++ dev/sample/so_5/news_board/main.cpp
@@ -3,12 +3,20 @@
  * different types of clients: news-writers and news-readers.
  */
 
-#include <iostream>
+#include <algorithm>
 #include <chrono>
 #include <ctime>
+#include <iostream>
+#include <iterator>
+#include <list>
 #include <random>
+#include <tuple>
 
 #include <so_5/all.hpp>
+
+//
+// Auxilary tools.
+//
 
 // Helper function to generate a random integer in the specified range.
 unsigned int
@@ -93,7 +101,7 @@ create_logger_coop( so_5::rt::environment_t & env )
 
 using story_id_type = unsigned long;
 
-// Base class for all messages. It necessary to store timestamp.
+// Base class for all messages. It stores timestamp.
 struct news_board_message_base : public so_5::rt::message_t
 	{
 		const clock_type::time_point m_timestamp;
@@ -103,10 +111,22 @@ struct news_board_message_base : public so_5::rt::message_t
 			{}
 	};
 
-// Request for publishing new story.
-struct msg_publish_story_req : public news_board_message_base
+// Base class for all request messages. It stores reply_to value.
+struct news_board_request_base : public news_board_message_base
 	{
 		const so_5::rt::mbox_t m_reply_to;
+
+		news_board_request_base(
+			clock_type::time_point timestamp,
+			so_5::rt::mbox_t reply_to )
+			:	news_board_message_base( std::move(timestamp) )
+			,	m_reply_to( std::move(reply_to) )
+			{}
+	};
+
+// Request for publishing new story.
+struct msg_publish_story_req : public news_board_request_base
+	{
 		const std::string m_title;
 		const std::string m_content;
 
@@ -115,8 +135,8 @@ struct msg_publish_story_req : public news_board_message_base
 			so_5::rt::mbox_t reply_to,
 			std::string title,
 			std::string content )
-			:	news_board_message_base( std::move(timestamp) )
-			,	m_reply_to( std::move(reply_to) )
+			:	news_board_request_base(
+					std::move(timestamp), std::move(reply_to) )
 			,	m_title( std::move(title) )
 			,	m_content( std::move(content) )
 			{}
@@ -132,6 +152,80 @@ struct msg_publish_story_resp : public news_board_message_base
 			story_id_type id )
 			:	news_board_message_base( std::move(timestamp) )
 			,	m_id( id )
+			{}
+	};
+
+// Request for updates from news board.
+struct msg_updates_req : public news_board_request_base
+	{
+		// Last known story ID.
+		story_id_type m_last_id;
+
+		msg_updates_req(
+			clock_type::time_point timestamp,
+			so_5::rt::mbox_t reply_to,
+			story_id_type last_id )
+			:	news_board_request_base(
+					std::move(timestamp), std::move(reply_to) )
+			,	m_last_id( last_id )
+			{}
+
+	};
+
+// Reply for request for updates.
+struct msg_updates_resp : public news_board_message_base
+	{
+		// Type of new stories list.
+		using story_list = std::list< std::tuple< story_id_type, std::string > >;
+
+		// List of short info about new storis.
+		const story_list m_updates;
+
+		msg_updates_resp(
+			clock_type::time_point timestamp,
+			story_list updates )
+			:	news_board_message_base( std::move(timestamp) )
+			,	m_updates( std::move(updates) )
+			{}
+	};
+
+// Request for content of the story.
+struct msg_story_content_req : public news_board_request_base
+	{
+		// Story ID.
+		const story_id_type m_id;
+
+		msg_story_content_req(
+			clock_type::time_point timestamp,
+			so_5::rt::mbox_t reply_to,
+			story_id_type id )
+			:	news_board_request_base(
+					std::move(timestamp), std::move(reply_to) )
+			,	m_id( id )
+			{}
+	};
+
+// Positive response to a request for story content.
+struct msg_story_content_resp_ack : public news_board_message_base
+	{
+		// Story content.
+		const std::string m_content;
+
+		msg_story_content_resp_ack(
+			clock_type::time_point timestamp,
+			std::string content )
+			:	news_board_message_base( std::move(timestamp) )
+			,	m_content( std::move(content) )
+			{}
+	};
+
+// Negative response to a request for story content.
+// This message is used when story is removed from the board.
+struct msg_story_content_resp_nack : public news_board_message_base
+	{
+		msg_story_content_resp_nack(
+			clock_type::time_point timestamp )
+			:	news_board_message_base( std::move(timestamp) )
 			{}
 	};
 
@@ -162,6 +256,7 @@ struct news_board_data
 // Agents to work with news board data.
 //
 
+// Agent for receiving and storing new stories to news board.
 void
 define_news_receiver_agent(
 	so_5::rt::agent_coop_t & coop,
@@ -182,7 +277,7 @@ define_news_receiver_agent(
 
 					// Log this fact.
 					log( logger_mbox,
-							"news_receiver",
+							"board.receiver",
 							"new story published, id=" + std::to_string( story_id ) +
 							", title=" + evt.m_title );
 
@@ -198,11 +293,98 @@ define_news_receiver_agent(
 							auto removed_id = board_data.m_stories.begin()->first;
 							board_data.m_stories.erase( board_data.m_stories.begin() );
 							log( logger_mbox,
-									"news_receiver",
+									"board.receiver",
 									"old story removed, id=" + std::to_string( removed_id ) );
 						}
 				} );
 	}
+
+// Agent for handling requests about updates on news board.
+void
+define_news_directory_agent(
+	so_5::rt::agent_coop_t & coop,
+	news_board_data & board_data,
+	const so_5::rt::mbox_t & board_mbox,
+	const so_5::rt::mbox_t & logger_mbox )
+	{
+		// Publisher should have priority higher that news_receiver agent.
+		coop.define_agent( coop.make_agent_context() + so_5::prio::p2 )
+			// It handles just one message.
+			.event( board_mbox,
+				[&board_data, logger_mbox]( const msg_updates_req & req )
+				{
+					log( logger_mbox,
+							"board.directory",
+							"request for updates received, last_id=" +
+								std::to_string( req.m_last_id ) );
+
+					// Searching for new stories for that request
+					// and building result list.
+					msg_updates_resp::story_list new_stories;
+					std::transform(
+							board_data.m_stories.upper_bound( req.m_last_id ),
+							std::end( board_data.m_stories ),
+							std::back_inserter( new_stories ),
+							[]( const news_board_data::story_map::value_type & v ) {
+								return std::make_tuple( v.first, v.second.m_title );
+							} );
+
+					log( logger_mbox,
+							"board.directory",
+							std::to_string( new_stories.size() ) + " new stories found" );
+
+					// Sending response.
+					so_5::send< msg_updates_resp >(
+							req.m_reply_to,
+							req.m_timestamp,
+							std::move(new_stories) );
+				} );
+	}
+
+// Agent for handling requests for story content.
+void
+define_story_extractor_agent(
+	so_5::rt::agent_coop_t & coop,
+	news_board_data & board_data,
+	const so_5::rt::mbox_t & board_mbox,
+	const so_5::rt::mbox_t & logger_mbox )
+	{
+		// Publisher should have priority higher that news_directory agent.
+		coop.define_agent( coop.make_agent_context() + so_5::prio::p3 )
+			// It handles just one message.
+			.event( board_mbox,
+				[&board_data, logger_mbox]( const msg_story_content_req & req )
+				{
+					log( logger_mbox,
+							"board.extractor",
+							"request for story content received, id=" +
+								std::to_string( req.m_id ) );
+
+					auto it = board_data.m_stories.find( req.m_id );
+					if( it != board_data.m_stories.end() )
+						{
+							log( logger_mbox,
+									"board.extractor",
+									"story {" + std::to_string( req.m_id ) + "} found" );
+
+							so_5::send< msg_story_content_resp_ack >(
+									req.m_reply_to,
+									req.m_timestamp,
+									it->second.m_content );
+						}
+					else
+						{
+							log( logger_mbox,
+									"board.extractor",
+									"story {" + std::to_string( req.m_id ) + "} NOT found" );
+
+							so_5::send< msg_story_content_resp_nack >(
+									req.m_reply_to,
+									req.m_timestamp );
+						}
+				} );
+	}
+
 
 so_5::rt::mbox_t
 create_board_coop(
@@ -230,13 +412,17 @@ create_board_coop(
 
 				define_news_receiver_agent(
 						coop, *board_data, board_mbox, logger_mbox );
+				define_news_directory_agent(
+						coop, *board_data, board_mbox, logger_mbox );
+				define_story_extractor_agent(
+						coop, *board_data, board_mbox, logger_mbox );
 			} );
 
 		return board_mbox;
 	}
 
 //
-// Story publishers and news readers.
+// Story publishers.
 //
 
 class story_publisher : public so_5::rt::agent_t
@@ -350,6 +536,206 @@ create_publisher_coop(
 			} );
 	}
 
+//
+// News readers.
+//
+
+class news_reader : public so_5::rt::agent_t
+	{
+		struct msg_time_for_updates : public so_5::rt::signal_t {};
+
+	public :
+		news_reader(
+			context_t ctx,
+			std::string reader_name,
+			so_5::rt::mbox_t board_mbox,
+			so_5::rt::mbox_t logger_mbox )
+			:	so_5::rt::agent_t( ctx )
+			,	m_name( std::move(reader_name) )
+			,	m_board_mbox( std::move(board_mbox) )
+			,	m_logger_mbox( std::move(logger_mbox) )
+			{}
+
+		virtual void
+		so_define_agent() override
+			{
+				this >>= st_sleeping;
+
+				st_sleeping.event< msg_time_for_updates >(
+						&news_reader::evt_time_for_updates );
+
+				st_await_updates.event(
+						&news_reader::evt_updates_received );
+
+				st_await_story_content.event(
+						&news_reader::evt_story_content );
+				st_await_story_content.event(
+						&news_reader::evt_story_not_found );
+			}
+
+		virtual void
+		so_evt_start() override
+			{
+				initiate_time_for_updates_signal();
+			}
+
+	private :
+		// The agent will wait 'msg_time_for_updates' signal in this state.
+		const so_5::rt::state_t st_sleeping = so_make_state();
+		// The agent will wait updates from news board in this state. 
+		const so_5::rt::state_t st_await_updates = so_make_state();
+		// The agent will wait story content in this state.
+		const so_5::rt::state_t st_await_story_content = so_make_state();
+
+		const std::string m_name;
+
+		const so_5::rt::mbox_t m_board_mbox;
+		const so_5::rt::mbox_t m_logger_mbox;
+
+		// ID of last received story from news board.
+		story_id_type m_last_id = 0;
+
+		// List a stories to be requested from news board.
+		msg_updates_resp::story_list m_stories_to_read;
+
+		void
+		initiate_time_for_updates_signal()
+			{
+				so_5::send_delayed_to_agent< msg_time_for_updates >(
+						*this,
+						std::chrono::milliseconds{ random_value( 500, 2500 ) } );
+			}
+
+		void
+		evt_time_for_updates()
+			{
+				request_updates();
+			}
+
+		void
+		evt_updates_received( const msg_updates_resp & resp )
+			{
+				log( m_logger_mbox,
+						m_name,
+						std::to_string( resp.m_updates.size() ) +
+							" updates received, took " +
+							std::to_string( ms_from_time( resp.m_timestamp ) ) +
+							"ms" );
+
+				if( resp.m_updates.empty() )
+					{
+						// Nothing new. We should sleep.
+						this >>= st_sleeping;
+						initiate_time_for_updates_signal();
+					}
+				else
+					{
+						this >>= st_await_story_content;
+						// Read no more than 3 latest stories.
+						unsigned int c = 0;
+						for( auto it = resp.m_updates.rbegin();
+								it != resp.m_updates.rend() && c != 3; ++it, ++c )
+							m_stories_to_read.push_front( *it );
+
+						request_story_content();
+					}
+			}
+
+		void
+		evt_story_content( const msg_story_content_resp_ack & resp )
+			{
+				const auto & id = std::get<0>( *std::begin(m_stories_to_read) );
+				const auto & title = std::get<1>( *std::begin(m_stories_to_read) );
+
+				log( m_logger_mbox,
+						m_name,
+						"read story {" + std::to_string( id ) + "} '" +
+							title + "': \"" + resp.m_content + "\", took " +
+							std::to_string( ms_from_time( resp.m_timestamp ) ) +
+							"ms" );
+
+				remove_current_story_and_read_next();
+			}
+
+		void
+		evt_story_not_found( const msg_story_content_resp_nack & resp )
+			{
+				const auto & id = std::get<0>( *std::begin(m_stories_to_read) );
+				const auto & title = std::get<1>( *std::begin(m_stories_to_read) );
+
+				log( m_logger_mbox,
+						m_name,
+						"unable to read story {" + std::to_string( id ) + "} '" +
+							title + "', took " +
+							std::to_string( ms_from_time( resp.m_timestamp ) ) +
+							"ms" );
+
+				remove_current_story_and_read_next();
+			}
+
+		void
+		request_updates()
+			{
+				log( m_logger_mbox,
+						m_name,
+						"requesting updates, last_id=" + std::to_string( m_last_id ) );
+				so_5::send< msg_updates_req >( m_board_mbox,
+						clock_type::now(),
+						so_direct_mbox(),
+						m_last_id );
+
+				this >>= st_await_updates;
+			}
+
+		void
+		request_story_content()
+			{
+				auto id = std::get<0>( *std::begin(m_stories_to_read) );
+
+				log( m_logger_mbox,
+						m_name,
+						"requesting story {" + std::to_string( id ) + "}" );
+
+				so_5::send< msg_story_content_req >(
+						m_board_mbox,
+						clock_type::now(),
+						so_direct_mbox(),
+						id );
+			}
+
+		void
+		remove_current_story_and_read_next()
+			{
+				m_last_id = std::get<0>( *std::begin(m_stories_to_read) );
+				m_stories_to_read.pop_front();
+
+				if( !m_stories_to_read.empty() )
+					request_story_content();
+				else
+					request_updates();
+			}
+	};
+
+void
+create_reader_coop(
+	so_5::rt::environment_t & env,
+	const so_5::rt::mbox_t & board_mbox,
+	const so_5::rt::mbox_t & logger_mbox )
+	{
+		// All readers will work on the same working thread.
+		env.introduce_coop(
+			so_5::disp::one_thread::create_private_disp( env )->binder(),
+			[&]( so_5::rt::agent_coop_t & coop )
+			{
+				for( int i = 0; i != 50; ++i )
+					coop.make_agent< news_reader >(
+							"reader" + std::to_string(i+1),
+							board_mbox,
+							logger_mbox );
+			} );
+	}
+
+
 void
 init( so_5::rt::environment_t & env )
 	{
@@ -357,6 +743,7 @@ init( so_5::rt::environment_t & env )
 		auto board_mbox = create_board_coop( env, logger_mbox );
 
 		create_publisher_coop( env, board_mbox, logger_mbox );
+		create_reader_coop( env, board_mbox, logger_mbox );
 	}
 
 int
