51e4c791f3a6f50385876899ea0870eefef9f300
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-08-19 12:16:39 +0000

    The first unit-tests for new prio_dedicated_threads::one_per_prio dispatcher.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.8--dev-trunk@1631 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/so_5/all.hpp
+++ dev/so_5/all.hpp
@@ -20,4 +20,5 @@
 #include <so_5/disp/adv_thread_pool/h/pub.hpp>
 #include <so_5/disp/prio_one_thread/strictly_ordered/h/pub.hpp>
 #include <so_5/disp/prio_one_thread/quoted_round_robin/h/pub.hpp>
+#include <so_5/disp/prio_dedicated_threads/one_per_prio/h/pub.hpp>
 
