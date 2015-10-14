012d92f3048397ead0867e38cbccd1444325385e
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-10-14 13:09:56 +0000

    Some refactoring of old unit tests for message delivery tracing.
    New unit-test for message delivery tracing in limitless MPSC mboxes.
    
    NOTE: it is broken now. A modification of subscription storages required.
    
    
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.9--dev-trunk--tracing@1755 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/msg_tracing/simple_msg_count_mpsc_no_limits/main.cpp
+++ dev/test/so_5/msg_tracing/simple_msg_count_mpsc_no_limits/main.cpp
@@ -1,5 +1,5 @@
 /*
- * A simple test for message delivery tracing.
+ * A simple test for message delivery tracing on limitless MPSC mbox.
  */
 
 #include <iostream>
@@ -14,33 +14,24 @@ struct finish : public so_5::rt::signal_t {};
 
 class a_test_t : public so_5::rt::agent_t
 {
-	struct dummy_msg { int m_i = 0; };
-
 public :
-	a_test_t( context_t ctx, so_5::rt::mbox_t data_mbox )
+	a_test_t( context_t ctx )
 		:	so_5::rt::agent_t{ ctx }
-		,	m_data_mbox{ std::move( data_mbox ) }
 	{}
 
 	virtual void
 	so_define_agent() override
 	{
-		so_set_delivery_filter( m_data_mbox, []( const dummy_msg & msg ) {
-				return 0 == msg.m_i;
-			} );
-
-		so_subscribe( m_data_mbox ).event< finish >( &a_test_t::evt_finish );
+		so_subscribe_self().event< finish >( &a_test_t::evt_finish );
 	}
 
 	virtual void
 	so_evt_start() override
 	{
-		so_5::send< finish >( m_data_mbox );
+		so_5::send< finish >( *this );
 	}
 
 private :
-	const so_5::rt::mbox_t m_data_mbox;
-
 	void
 	evt_finish()
 	{
@@ -52,7 +43,7 @@ void
 init( so_5::rt::environment_t & env )
 {
 	env.introduce_coop( []( so_5::rt::agent_coop_t & coop ) {
-			coop.make_agent< a_test_t >( coop.environment().create_local_mbox() );
+			coop.make_agent< a_test_t >();
 		} );
 }
 
@@ -73,7 +64,7 @@ main()
 												so_5::msg_tracing::std_cout_tracer() } } );
 					} );
 
-				const unsigned int expected_value = 8;
+				const unsigned int expected_value = 6;
 				auto actual_value = counter.load( std::memory_order_acquire );
 				if( expected_value != actual_value )
 					throw std::runtime_error( "Unexpected count of trace messages: "
@@ -81,7 +72,7 @@ main()
 							", actual=" + std::to_string(actual_value) );
 			},
 			4,
-			"simple tracing for MPMC-mboxes" );
+			"simple tracing for limitless MPSC-mboxes" );
 	}
 	catch( const std::exception & ex )
 	{
--- dev/test/so_5/msg_tracing/simple_svc_count/main.cpp
+++ dev/test/so_5/msg_tracing/simple_svc_count/main.cpp
@@ -3,40 +3,12 @@
  */
 
 #include <iostream>
-#include <map>
-#include <exception>
-#include <stdexcept>
-#include <cstdlib>
-#include <thread>
-#include <chrono>
 
 #include <so_5/all.hpp>
 
 #include <various_helpers_1/time_limited_execution.hpp>
 
-using counter_t = std::atomic< unsigned int >;
-
-class tracer_t : public so_5::msg_tracing::tracer_t
-{
-public :
-	tracer_t(
-		counter_t & counter,
-		so_5::msg_tracing::tracer_unique_ptr_t actual_tracer )
-		:	m_counter{ counter }
-		,	m_actual_tracer{ std::move( actual_tracer ) }
-	{}
-
-	virtual void
-	trace( const std::string & message ) SO_5_NOEXCEPT override
-	{
-		++m_counter;
-		m_actual_tracer->trace( message );
-	}
-
-private :
-	counter_t & m_counter;
-	so_5::msg_tracing::tracer_unique_ptr_t m_actual_tracer;
-};
+#include "../simple_tracer.hpp"
 
 struct finish : public so_5::rt::signal_t {};
 
