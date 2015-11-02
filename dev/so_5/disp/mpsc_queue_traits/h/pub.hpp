771dc02036d5fab49ea2705436b55df3167f7406
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-11-02 08:25:40 +0000

    The very first step in implementation of so_5::disp::mpsc_queue_traits.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.10--dev-trunk--combined-lock@1814 1d1731ae-3f85-447d-88f8-c72b288064d1

---
+++ dev/so_5/disp/mpsc_queue_traits/h/pub.hpp
@@ -0,0 +1,184 @@
+/*
+ * SObjectizer 5
+ */
+
+/*!
+ * \since v.5.5.10
+ * \file
+ * \brief Various traits for MPSC queues.
+ */
+
+#pragma once
+
+#include <so_5/h/declspec.hpp>
+
+#include <functional>
+#include <memory>
+#include <chrono>
+
+namespace so_5 {
+
+namespace disp {
+
+namespace mpsc_queue_traits {
+
+//
+// lock_t
+//
+/*!
+ * \since v.5.5.10
+ * \brief An interface for lock for MPSC queue.
+ */
+class SO_5_TYPE lock_t
+	{
+		friend class unique_lock_t;
+		friend class lock_guard_t;
+
+	public :
+		lock_t();
+		lock_t( const lock_t & ) = delete;
+		lock_t( lock_t && ) = delete;
+		virtual ~lock_t();
+
+		//! Lock object in exclusive mode.
+		virtual void
+		lock() = 0;
+
+		//! Unlock object locked in exclusive mode.
+		virtual void
+		unlock() = 0;
+
+	protected :
+		//! Waiting for nofication.
+		/*!
+		 * \attention Must be called only when object is locked!
+		 */
+		virtual void
+		wait_for_notify() = 0;
+
+		//! Notify one waiting thread if it exists.
+		/*!
+		 * \attention Must be called only when object is locked.
+		 */
+		virtual void
+		notify_one() = 0;
+	};
+
+//
+// lock_unique_ptr_t
+//
+/*!
+ * \since v.5.5.10
+ * \brief An alias for unique_ptr for lock.
+ */
+using lock_unique_ptr_t = std::unique_ptr< lock_t >; 
+
+//
+// lock_factory_t
+//
+/*!
+ * \since v.5.5.10
+ * \brief Type of lock factory.
+ */
+using lock_factory_t = std::function< lock_unique_ptr_t() >;
+
+//
+// combined_lock_factory
+//
+//FIXME: example of usage is necessary in Doxygen-comment.
+/*!
+ * \since v.5.5.10
+ * \brief Factory for creation of combined queue lock with default waiting time.
+ */
+SO_5_FUNC lock_factory_t
+combined_lock_factory();
+
+//FIXME: example of usage is necessary in Doxygen-comment.
+/*!
+ * \since v.5.5.10
+ * \brief Factory for creation of combined queue lock with the specified
+ * waiting time.
+ */
+SO_5_FUNC lock_factory_t
+combined_lock_factory(
+	//! Max waiting time for waiting on spinlock before switching to mutex.
+	std::chrono::high_resolution_clock::duration waiting_time );
+
+//
+// unique_lock_t
+//
+/*!
+ * \since v.5.5.10
+ * \brief An analog of std::unique_lock for MPSC queue lock.
+ */
+class unique_lock_t
+	{
+	public :
+		inline
+		unique_lock_t( lock_t & lock )
+			:	m_lock( lock )
+			{
+				m_lock.lock();
+			}
+
+		inline
+		~unique_lock_t()
+			{
+				m_lock.unlock();
+			}
+
+		unique_lock_t( const unique_lock_t & ) = delete;
+		unique_lock_t( unique_lock_t && ) = delete;
+
+		inline void
+		wait_for_notify()
+			{
+				m_lock.wait_for_notify();
+			}
+
+	private :
+		lock_t & m_lock;
+	};
+
+//
+// lock_guard_t
+//
+/*!
+ * \since v.5.4.0
+ * \brief An analog of std::lock_guard for MPSC queue lock.
+ */
+class lock_guard_t
+	{
+	public :
+		inline
+		lock_guard_t(
+			lock_t & lock )
+			:	m_lock( lock )
+			{
+				m_lock.lock();
+			}
+		inline
+		~lock_guard_t()
+			{
+				m_lock.unlock();
+			}
+
+		lock_guard_t( const lock_guard_t & ) = delete;
+		lock_guard_t( lock_guard_t && ) = delete;
+
+		inline void
+		notify_one()
+			{
+				m_lock.notify_one();
+			}
+
+	private :
+		lock_t & m_lock;
+	};
+
+} /* namespace mpsc_queue_traits */
+
+} /* namespace disp */
+
+} /* namespace so_5 */
+
---
+++ dev/so_5/disp/mpsc_queue_traits/pub.cpp
@@ -0,0 +1,172 @@
+/*
+ * SObjectizer 5
+ */
+
+/*!
+ * \since v.5.5.10
+ * \file
+ * \brief Various traits for MPSC queues.
+ */
+
+#include <so_5/disp/mpsc_queue_traits/h/pub.hpp>
+
+#include <so_5/h/spinlocks.hpp>
+
+#include <mutex>
+#include <condition_variable>
+
+namespace so_5 {
+
+namespace disp {
+
+namespace mpsc_queue_traits {
+
+//
+// lock_t
+//
+lock_t::lock_t() {}
+lock_t::~lock_t() {}
+
+namespace impl {
+
+//
+// combined_lock_t
+//
+/*!
+ * \since v.5.5.10
+ * \brief A special combined lock for queue protection.
+ *
+ * This lock used spinlocks for efficiency and std::mutex and
+ * std::condition_variable for signalization.
+ *
+ * \attention This lock can be used only for single-consumer queues!
+ * It is because there is no way found to implement notify_all on
+ * just two int variables (m_waiting and m_signaled). 
+ */
+class combined_lock_t : public lock_t
+	{
+	public :
+		inline
+		combined_lock_t(
+			//! Max waiting time for waiting on spinlock before switching to mutex.
+			std::chrono::high_resolution_clock::duration waiting_time )
+			:	m_waiting_time{ waiting_time }
+			,	m_waiting( false )
+			,	m_signaled( false )
+			{}
+
+		combined_lock_t( const combined_lock_t & ) = delete;
+		combined_lock_t( combined_lock_t && ) = delete;
+
+		virtual void
+		lock() override
+			{
+				m_spinlock.lock();
+			}
+
+		virtual void
+		unlock() override
+			{
+				m_spinlock.unlock();
+			}
+
+	protected :
+		virtual void
+		wait_for_notify() override
+			{
+				using clock = std::chrono::high_resolution_clock;
+
+				m_waiting = true;
+				auto stop_point = clock::now() + m_waiting_time;
+
+				do
+					{
+						m_spinlock.unlock();
+
+						std::this_thread::yield();
+
+						m_spinlock.lock();
+
+						if( m_signaled )
+							{
+								m_waiting = false;
+								m_signaled = false;
+								return;
+							}
+					}
+				while( stop_point > clock::now() );
+
+				// m_lock is locked now.
+
+				// Must use heavy std::mutex and std::condition_variable
+				// to allow OS to efficiently use the resources while
+				// we are waiting for signal.
+				std::unique_lock< std::mutex > mlock( m_mutex );
+
+				m_spinlock.unlock();
+
+				m_condition.wait( mlock, [this]{ return m_signaled; } );
+
+				// At this point m_signaled must be 'true'.
+
+				m_spinlock.lock();
+
+				m_waiting = false;
+				m_signaled = false;
+			}
+
+		//! Notify one waiting thread if it exists.
+		/*!
+		 * \attention Must be called only when object is locked.
+		 */
+		virtual void
+		notify_one() override
+			{
+				if( m_waiting )
+					{
+						// There is a waiting thread.
+						m_mutex.lock();
+						m_signaled = true;
+						m_condition.notify_one();
+						m_mutex.unlock();
+					}
+			}
+
+	private :
+		const std::chrono::high_resolution_clock::duration m_waiting_time;
+
+		default_spinlock_t m_spinlock;
+
+		std::mutex m_mutex;
+		std::condition_variable m_condition;
+
+		bool m_waiting;
+		bool m_signaled;
+	};
+
+} /* namespace impl */
+
+//
+// combined_lock_factory
+//
+SO_5_FUNC lock_factory_t
+combined_lock_factory()
+	{
+		return combined_lock_factory( std::chrono::milliseconds{1} );
+	}
+
+SO_5_FUNC lock_factory_t
+combined_lock_factory(
+	std::chrono::high_resolution_clock::duration waiting_time )
+	{
+		return [waiting_time] {
+			return lock_unique_ptr_t{ new impl::combined_lock_t{ waiting_time } };
+		};
+	}
+
+} /* namespace mpsc_queue_traits */
+
+} /* namespace disp */
+
+} /* namespace so_5 */
+
