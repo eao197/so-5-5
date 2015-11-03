0c3697dc31ef165e0b7081c677a6cddccf5531be
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-11-03 10:29:55 +0000

    Unit-test modified to check different locks by using different lock factories.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.10--dev-trunk--combined-lock@1817 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/spinlocks/combined_queue_lock/main.cpp
+++ dev/test/so_5/spinlocks/combined_queue_lock/main.cpp
@@ -14,10 +14,12 @@
 using namespace so_5::disp::mpsc_queue_traits;
 
 void
-test_thread( unsigned int milliseconds_to_wait )
+test_thread(
+	lock_factory_t factory,
+	unsigned int milliseconds_to_wait )
 {
-	lock_unique_ptr_t lock_a{ combined_lock_factory()() };
-	lock_unique_ptr_t lock_b{ combined_lock_factory()() };
+	lock_unique_ptr_t lock_a{ factory() };
+	lock_unique_ptr_t lock_b{ factory() };
 
 	unique_lock_t guard_b{ *lock_b };
 
@@ -46,18 +48,20 @@ test_thread( unsigned int milliseconds_to_wait )
 }
 
 void
-do_pair_thread_test_case( unsigned int milliseconds_to_wait )
+do_pair_thread_test_case(
+	lock_factory_t factory,
+	unsigned int milliseconds_to_wait )
 {
-	std::thread parent{ test_thread, milliseconds_to_wait };
+	std::thread parent{ test_thread, factory, milliseconds_to_wait };
 	parent.join();
 }
 
 void
-single_pair_test_case()
+single_pair_test_case( lock_factory_t factory )
 {
-	run_with_time_limit( [] {
+	run_with_time_limit( [factory] {
 			std::cout << "single pair: " << std::flush;
-			do_pair_thread_test_case( 250 );
+			do_pair_thread_test_case( factory, 250 );
 			std::cout << "OK" << std::endl;
 		},
 		2,
@@ -66,14 +70,14 @@ single_pair_test_case()
 }
 
 void
-serie_of_pair_tests_with_equal_intervals()
+serie_of_pair_tests_with_equal_intervals( lock_factory_t factory )
 {
 	std::cout << "serie of pair tests with equal interval: " << std::flush;
 
 	for( int i = 0; i != 50; ++i )
 	{
-		run_with_time_limit( [] {
-			do_pair_thread_test_case( 25 );
+		run_with_time_limit( [factory] {
+			do_pair_thread_test_case( factory, 25 );
 		},
 		2,
 		"serie_of_pair_tests_with_equal_intervals" );
@@ -84,14 +88,14 @@ serie_of_pair_tests_with_equal_intervals()
 }
 
 void
-serie_of_pair_tests_with_different_intervals()
+serie_of_pair_tests_with_different_intervals( lock_factory_t factory )
 {
 	std::cout << "serie of pair tests with different intervals: " << std::flush;
 
 	for( int i = 1; i != 50; ++i )
 	{
-		run_with_time_limit( [i] {
-			do_pair_thread_test_case( i*5 );
+		run_with_time_limit( [factory, i] {
+			do_pair_thread_test_case( factory, i*5 );
 		},
 		2,
 		"serie_of_pair_tests_with_different_intervals" );
@@ -102,7 +106,7 @@ serie_of_pair_tests_with_different_intervals()
 }
 
 void
-bunch_of_threads()
+bunch_of_threads( lock_factory_t factory )
 {
 	std::cout << "bunch of threads: " << std::flush;
 
@@ -111,7 +115,7 @@ bunch_of_threads()
 
 	run_with_time_limit( [&] {
 			for( std::size_t i = 0; i != 50; ++i )
-				threads.push_back( std::thread{ test_thread, 250u } );
+				threads.push_back( std::thread{ test_thread, factory, 250u } );
 
 			for( auto & t : threads )
 				t.join();
@@ -127,10 +131,28 @@ main()
 {
 	try
 	{
-		single_pair_test_case();
-		serie_of_pair_tests_with_equal_intervals();
-		serie_of_pair_tests_with_different_intervals();
-		bunch_of_threads();
+		struct case_info_t
+		{
+			std::string m_name;
+			lock_factory_t m_factory;
+		};
+
+		std::vector< case_info_t > cases;
+		cases.push_back( case_info_t{ "combined_lock(default)", combined_lock_factory() } );
+		cases.push_back( case_info_t{ "combined_lock(1us)",
+				combined_lock_factory( std::chrono::microseconds(1) ) } );
+
+		for( const auto & c : cases )
+		{
+			std::cout << "CHECKING CASE: " << c.m_name << "..." << std::endl;
+
+			single_pair_test_case( c.m_factory );
+			serie_of_pair_tests_with_equal_intervals( c.m_factory );
+			serie_of_pair_tests_with_different_intervals( c.m_factory );
+			bunch_of_threads( c.m_factory );
+
+			std::cout << "--- DONE ---" << std::endl;
+		}
 
 		return 0;
 	}
