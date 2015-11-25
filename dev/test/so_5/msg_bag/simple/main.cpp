9da2744f114d789589ce2785cc0cc640dc9d85be
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-11-25 15:59:24 +0000

    Simple unit-test for msg_bag expanded for all types of msg_bag storage.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.13--dev-trunk--msg-bag@1916 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/msg_bag/simple/main.cpp
+++ dev/test/so_5/msg_bag/simple/main.cpp
@@ -6,6 +6,13 @@
 
 #include <various_helpers_1/time_limited_execution.hpp>
 
+#include <vector>
+#include <utility>
+#include <string>
+
+using namespace std;
+namespace bag_ns = so_5::rt::msg_bag;
+
 int
 main()
 {
@@ -16,34 +23,68 @@ main()
 			{
 				so_5::wrapped_env_t env;
 
-				auto bag = env.environment().create_msg_bag(
+				vector< pair< string, so_5::rt::bag_params_t > > params;
+				params.emplace_back( "unlimited",
 						so_5::rt::bag_params_t{
-								so_5::rt::msg_bag::capacity_t::make_unlimited() } );
+								bag_ns::capacity_t::make_unlimited() } );
+				params.emplace_back( "limited(dynamic,nowait)",
+						so_5::rt::bag_params_t{
+								bag_ns::capacity_t::make_limited_without_waiting(
+										5,
+										bag_ns::storage_memory_t::dynamic,
+										bag_ns::overflow_reaction_t::drop_newest ) } );
+				params.emplace_back( "limited(preallocated,nowait)",
+						so_5::rt::bag_params_t{
+								bag_ns::capacity_t::make_limited_without_waiting(
+										5,
+										bag_ns::storage_memory_t::preallocated,
+										bag_ns::overflow_reaction_t::drop_newest ) } );
+				params.emplace_back( "limited(dynamic,wait)",
+						so_5::rt::bag_params_t{
+								bag_ns::capacity_t::make_limited_with_waiting(
+										5,
+										bag_ns::storage_memory_t::dynamic,
+										bag_ns::overflow_reaction_t::drop_newest,
+										chrono::milliseconds(200) ) } );
+				params.emplace_back( "limited(preallocated,wait)",
+						so_5::rt::bag_params_t{
+								bag_ns::capacity_t::make_limited_with_waiting(
+										5,
+										bag_ns::storage_memory_t::preallocated,
+										bag_ns::overflow_reaction_t::drop_newest,
+										chrono::milliseconds(200) ) } );
 
-				so_5::send< int >( bag->as_mbox(), 42 );
+				for( const auto & p : params )
+				{
+					cout << "=== " << p.first << " ===" << endl;
 
-				auto r = so_5::receive(
-						bag,
-						so_5::rt::msg_bag::clock::duration::zero(),
-						so_5::handler( []( int i ) {
-							if( 42 != i )
-								throw std::runtime_error( "unexpected int-message: "
-										+ std::to_string( i ) );
-						} ),
-						so_5::handler( []( const std::string & s ) {
-							throw std::runtime_error( "unexpected string msg: " + s );
-						} ) );
+					auto bag = env.environment().create_msg_bag( p.second );
 
-				if( r != 1 )
-					throw std::runtime_error( "unexpected value of so_5::receive "
-							"return code: " + std::to_string( r ) );
+					so_5::send< int >( bag->as_mbox(), 42 );
+
+					auto r = so_5::receive(
+							bag,
+							so_5::rt::msg_bag::clock::duration::zero(),
+							so_5::handler( []( int i ) {
+								if( 42 != i )
+									throw runtime_error( "unexpected int-message: "
+											+ to_string( i ) );
+							} ),
+							so_5::handler( []( const std::string & s ) {
+								throw runtime_error( "unexpected string msg: " + s );
+							} ) );
+
+					if( r != 1 )
+						throw runtime_error( "unexpected value of so_5::receive "
+								"return code: " + to_string( r ) );
+				}
 			},
 			4,
 			"simple test for msg_bag" );
 	}
-	catch( const std::exception & ex )
+	catch( const exception & ex )
 	{
-		std::cerr << "Error: " << ex.what() << std::endl;
+		cerr << "Error: " << ex.what() << endl;
 		return 1;
 	}
 
