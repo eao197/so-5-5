3621eae5571b4729111eb649a46c9bebcf8b8b3e
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-11-28 10:17:06 +0000

    Unit-test for so_5::receive(chain, so_5::infinite_wait,...) added.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.13--dev-trunk--msg-bag@1936 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/mchain/build_tests.rb
+++ dev/test/so_5/mchain/build_tests.rb
@@ -5,6 +5,7 @@ MxxRu::Cpp::composite_target {
 
 	path = 'test/so_5/mchain'
 
+	required_prj( "#{path}/infinite_wait/prj.ut.rb" )
 	required_prj( "#{path}/simple/prj.ut.rb" )
 	required_prj( "#{path}/simple_svc/prj.ut.rb" )
 	required_prj( "#{path}/close_chain/prj.ut.rb" )
