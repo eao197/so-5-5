0737dd90b0ba78054dbc2dd62efb6265a435920c
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-11-29 13:51:35 +0000

    Unit-test for advanced receive extended.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.13--dev-trunk--msg-bag@1940 1d1731ae-3f85-447d-88f8-c72b288064d1

--- dev/test/so_5/mchain/adv_receive/main.cpp
+++ dev/test/so_5/mchain/adv_receive/main.cpp
@@ -63,7 +63,7 @@ do_check_total_time( const so_5::mchain & chain )
 		UT_CHECK_CONDITION( 3 == r.extracted() );
 		UT_CHECK_CONDITION( 1 == r.handled() );
 		UT_CHECK_CONDITION(
-				so_5::mchain_props::extraction_status::no_messages == r.status() );
+				so_5::mchain_props::extraction_status::msg_extracted == r.status() );
 	} };
 
 	child.join();
@@ -81,11 +81,114 @@ UT_UNIT_TEST( test_total_time )
 			{
 				so_5::wrapped_env_t env;
 
-				do_check_timeout_on_empty_queue(
+				do_check_total_time(
 						env.environment().create_mchain( p.second ) );
 			},
 			4,
-			"test_timeout_on_empty_queue: " + p.first );
+			"test_total_time: " + p.first );
+	}
+}
+
+void
+do_check_handle_n(
+	const so_5::mchain & ch1,
+	const so_5::mchain & ch2 )
+{
+	std::thread child{ [&] {
+		auto r = receive(
+				from( ch1 ).handle_n( 3 ),
+				so_5::handler( [&ch2]( int i ) {
+					so_5::send< int >( ch2->as_mbox(), i );
+				} ) );
+
+		UT_CHECK_CONDITION( 3 == r.extracted() );
+		UT_CHECK_CONDITION( 3 == r.handled() );
+	} };
+
+	so_5::send< int >( ch1->as_mbox(), 0 );
+	auto r = receive(
+			from( ch2 ).handle_n( 2 ),
+			so_5::handler( [&ch1]( int i ) {
+				so_5::send< int >( ch1->as_mbox(), i + 1 );
+			} ) );
+
+	UT_CHECK_CONDITION( 2 == r.extracted() );
+	UT_CHECK_CONDITION( 2 == r.handled() );
+
+	child.join();
+}
+
+UT_UNIT_TEST( test_handle_n )
+{
+	auto params = build_mchain_params();
+	for( const auto & p : params )
+	{
+		cout << "=== " << p.first << " ===" << endl;
+
+		run_with_time_limit(
+			[&p]()
+			{
+				so_5::wrapped_env_t env;
+
+				do_check_handle_n(
+						env.environment().create_mchain( p.second ),
+						env.environment().create_mchain( p.second ) );
+			},
+			4,
+			"test_handle_n: " + p.first );
+	}
+}
+
+void
+do_check_extract_n(
+	const so_5::mchain & ch1,
+	const so_5::mchain & ch2 )
+{
+	std::thread child{ [&] {
+		auto r = receive(
+				from( ch1 ).handle_n( 3 ).extract_n( 3 ),
+				so_5::handler( [&ch2]( int i ) {
+					so_5::send< int >( ch2->as_mbox(), i );
+				} ) );
+
+		UT_CHECK_CONDITION( 3 == r.extracted() );
+		UT_CHECK_CONDITION( 1 == r.handled() );
+	} };
+
+	so_5::send< string >( ch1->as_mbox(), "0" );
+	so_5::send< int >( ch1->as_mbox(), 0 );
+
+	auto r = receive(
+			from( ch2 ).handle_n( 1 ),
+			so_5::handler( [&ch1]( int i ) {
+				so_5::send< string >( ch1->as_mbox(), to_string( i + 1 ) );
+				so_5::send< int >( ch1->as_mbox(), i + 1 );
+			} ) );
+
+	UT_CHECK_CONDITION( 1 == r.extracted() );
+	UT_CHECK_CONDITION( 1 == r.handled() );
+
+	child.join();
+}
+
+UT_UNIT_TEST( test_extract_n )
+{
+	auto params = build_mchain_params();
+	for( const auto & p : params )
+	{
+		cout << "=== " << p.first << " ===" << endl;
+
+		run_with_time_limit(
+			[&p]()
+			{
+				so_5::wrapped_env_t env;
+
+				do_check_extract_n(
+						env.environment().create_mchain( p.second ),
+						env.environment().create_mchain( p.second ) );
+			},
+			4,
+			"test_extract_n: " + p.first );
 	}
 }
 
@@ -94,6 +197,8 @@ main()
 {
 	UT_RUN_UNIT_TEST( test_timeout_on_empty_queue )
 	UT_RUN_UNIT_TEST( test_total_time )
+	UT_RUN_UNIT_TEST( test_handle_n )
+	UT_RUN_UNIT_TEST( test_extract_n )
 
 	return 0;
 }
