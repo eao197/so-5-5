9066e769304bd86adbeaf3b4c69c7ce8510fe4fe
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-11-30 08:38:43 +0000

    The very first sketch of unit-test for checking size-limited mchains.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/5.5.13--dev-trunk--msg-bag@1944 1d1731ae-3f85-447d-88f8-c72b288064d1

---
+++ dev/test/so_5/mchain/limited_no_app_abort/main.cpp
@@ -0,0 +1,143 @@
+/*
+ * Test for size-limited mchains without abort_app overflow reaction.
+ */
+
+#include <so_5/all.hpp>
+
+#include <various_helpers_1/time_limited_execution.hpp>
+
+#include <utest_helper_1/h/helper.hpp>
+
+using namespace std;
+using namespace chrono;
+
+namespace props = so_5::mchain_props;
+
+void
+do_check_drop_newest_impl(
+	so_5::rt::environment_t & env,
+	const char * case_name,
+	props::storage_memory memory )
+{
+	cout << "no_wait, drop_newest, " << case_name << ": " << std::flush;
+
+	auto ch = env.create_mchain( so_5::mchain_params{
+			props::capacity::make_limited_without_waiting(
+					3,
+					memory,
+					props::overflow_reaction::drop_newest ) } );
+
+	so_5::send< int >( ch, 1 );
+	so_5::send< int >( ch, 2 );
+	so_5::send< int >( ch, 3 );
+	so_5::send< int >( ch, 4 );
+
+	receive( from(ch).handle_n(4).empty_timeout(so_5::no_wait),
+			[]( int i ) { UT_CHECK_CONDITION( i < 4 ); } );
+
+	cout << "OK" << std::endl;
+}
+
+void
+do_check_drop_newest( so_5::rt::environment_t & env )
+{
+	do_check_drop_newest_impl(
+			env, "dynamic", props::storage_memory::dynamic );
+	do_check_drop_newest_impl(
+			env, "prealloc", props::storage_memory::preallocated );
+}
+
+void
+do_check_remove_oldest_impl(
+	so_5::rt::environment_t & env,
+	const char * case_name,
+	props::storage_memory memory )
+{
+	cout << "no_wait, remove_oldest, " << case_name << ": " << std::flush;
+
+	auto ch = env.create_mchain( so_5::mchain_params{
+			props::capacity::make_limited_without_waiting(
+					3,
+					memory,
+					props::overflow_reaction::remove_oldest ) } );
+
+	so_5::send< int >( ch, 1 );
+	so_5::send< int >( ch, 2 );
+	so_5::send< int >( ch, 3 );
+	so_5::send< int >( ch, 4 );
+
+	receive( from(ch).handle_n(4).empty_timeout(so_5::no_wait),
+			[]( int i ) { UT_CHECK_CONDITION( i > 1 ); } );
+
+	cout << "OK" << std::endl;
+}
+
+void
+do_check_remove_oldest( so_5::rt::environment_t & env )
+{
+	do_check_remove_oldest_impl(
+			env, "dynamic", props::storage_memory::dynamic );
+	do_check_remove_oldest_impl(
+			env, "prealloc", props::storage_memory::preallocated );
+}
+
+void
+do_check_throw_exception_impl(
+	so_5::rt::environment_t & env,
+	const char * case_name,
+	props::storage_memory memory )
+{
+	cout << "no_wait, throw_exception, " << case_name << ": " << std::flush;
+
+	auto ch = env.create_mchain( so_5::mchain_params{
+			props::capacity::make_limited_without_waiting(
+					3,
+					memory,
+					props::overflow_reaction::throw_exception ) } );
+
+	so_5::send< int >( ch, 1 );
+	so_5::send< int >( ch, 2 );
+	so_5::send< int >( ch, 3 );
+	try
+	{
+		so_5::send< int >( ch, 4 );
+		UT_CHECK_CONDITION( !"An exception must be throw before this line!" );
+	}
+	catch( const so_5::exception_t & ex )
+	{
+		UT_CHECK_CONDITION( so_5::rc_msg_chain_overflow == ex.error_code() );
+	}
+
+	cout << "OK" << std::endl;
+}
+
+void
+do_check_throw_exception( so_5::rt::environment_t & env )
+{
+	do_check_throw_exception_impl(
+			env, "dynamic", props::storage_memory::dynamic );
+	do_check_throw_exception_impl(
+			env, "prealloc", props::storage_memory::preallocated );
+}
+
+UT_UNIT_TEST( test_no_wait )
+{
+	run_with_time_limit(
+		[] {
+			so_5::wrapped_env_t env;
+
+			do_check_drop_newest( env.environment() );
+			do_check_remove_oldest( env.environment() );
+			do_check_throw_exception( env.environment() );
+		},
+		4,
+		"test_no_wait" );
+}
+int
+main()
+{
+	UT_RUN_UNIT_TEST( test_no_wait )
+
+	return 0;
+}
+
