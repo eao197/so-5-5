b888e8ed11d6ae8d97303c671c83a8837ee7e584
eao197 <eao197@1d1731ae-3f85-447d-88f8-c72b288064d1>
2015-12-28 19:37:04 +0000

    New unit-tests added to build_tests.
    git-svn-id: svn://svn.code.sf.net/p/sobjectizer/repo/branches/so_5/prototypes/new-statecharts@2094 1d1731ae-3f85-447d-88f8-c72b288064d1

---
+++ dev/test/so_5/state/build_tests.rb
@@ -0,0 +1,13 @@
+#!/usr/local/bin/ruby
+require 'mxx_ru/cpp'
+
+path = 'test/so_5/state'
+
+MxxRu::Cpp::composite_target {
+
+	required_prj "#{path}/change_state/prj.ut.rb"
+	required_prj "#{path}/composite_state_change/prj.ut.rb"
+	required_prj "#{path}/enter_exit_handlers/prj.ut.rb"
+	required_prj "#{path}/nesting_deep/prj.ut.rb"
+	required_prj "#{path}/parent_state_handler/prj.ut.rb"
+}
